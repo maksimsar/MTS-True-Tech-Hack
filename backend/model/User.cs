09491b155c8c9dbf1016f29b670f01990fe29143
Maxim <maks.degtearyov25@gmail.com>
2025-04-18 22:52:49 +0300

    Squashed commit of the following:
    commit c4f63358976133bb1ae4a38daec23797fc2565f3
    Author: Maxim <maks.degtearyov25@gmail.com>
    Date:   Fri Apr 18 21:41:01 2025 +0300
    
        Игнорируем сборочные артефакты
    
    commit eea0e7139fcd2b34e410b1a45e49324da4556c6c
    Merge: cbebc96 189571f
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 21:38:08 2025 +0300
    
        слил в dev
    
    commit cbebc961fad34681b7c2c390ad5d00ab10ec5bb0
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 21:37:21 2025 +0300
    
        aborting
    
    commit 189571fdf5aee6b0386a46f2bd038f38d57d536c
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 21:35:41 2025 +0300
    
        Игнорируем сборочные артефакты
    
    commit fb2be2e9611a733c00afeb5136b24f1e6a36102e
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 19:00:24 2025 +0300
    
        Обновлён AppDbContext с детальной конфигурацией Fluent API
    
    commit 105c97f9ebbc83f5ecf9317e52b5520b556e1426
    Merge: 82daf6a 686c4e2
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 18:52:52 2025 +0300
    
        Исправлены ошибки в appsettings.json, AppDbContext.cs, Program.cs и моделях
    
    commit 82daf6a176e5407d5520cba5110225604875a6b1
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 18:10:41 2025 +0300
    
        aborting
    
    commit 686c4e27b1455b419d73b8e094a9b98502e4d7b0
    Merge: 996ae51 f6e1657
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 18:09:50 2025 +0300
    
        aborting
    
    commit 2861e2c188a424bdb0ba852d670692d5bf1f050d
    Merge: e75566f 996ae51
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 18:08:14 2025 +0300
    
        aborting
    
    commit f6e16579e6626cb7d23c5cf02869ce65b9940183
    Merge: 7a5d7ee ec3db1d
    Author: AnnMazur <mazur20ann@yandex.ru>
    Date:   Fri Apr 18 17:09:34 2025 +0300
    
        настройка проекта + наброски фронтенда
    
    commit ec3db1da1b7f5be3b6453e294f35cba1610d53fb
    Author: AnnMazur <mazur20ann@yandex.ru>
    Date:   Fri Apr 18 16:30:23 2025 +0300
    
        настройка проекта + наброски фронтенда
    
    commit 7a5d7ee1b10997159f56d1641369fc1e147bbcb0
    Author: Maxim <maks.degtearyov25@gmail.com>
    Date:   Fri Apr 18 14:53:06 2025 +0300
    
        deleye weather
    
    commit e75566f60fe61b45eb010abe73da8be1a993f3b7
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 14:40:35 2025 +0300
    
        aborting
    
    commit 996ae517355ba93546d4defae04980e2ca855a3b
    Merge: 736aa65 d1a3807
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 14:39:33 2025 +0300
    
        перенес изменения
    
    commit 736aa652c6957663f2dec1fb749026ece2602dbc
    Author: ITYSHKANCHIKI <shkenev05@bk.ru>
    Date:   Fri Apr 18 14:37:32 2025 +0300
    
        склонировал
    
    commit d1a3807bbe9c49d1929383cc9001b17c78874580
    Author: Maxim <maks.degtearyov25@gmail.com>
    Date:   Fri Apr 18 14:31:49 2025 +0300
    
        Squashed commit of the following:
    
        commit 374f347ccb9b27bf0a4201fef865fa5a89d89857
        Author: Maxim <maks.degtearyov25@gmail.com>
        Date:   Fri Apr 18 14:28:13 2025 +0300
    
            Delete .gitignore
    
        commit eccf99bc91c3cbb00fbaad380728108ceeaf7ce7
        Author: Maxim <maks.degtearyov25@gmail.com>
        Date:   Fri Apr 18 14:27:01 2025 +0300
    
            гнорируем сборочные артефакты и NuGet‑файлы

---
+++ backend/model/User.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+namespace MTSTrueTechHack.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+
+        public List<Schema> Schemas { get; set; } = new List<Schema>();
+    }
+}
